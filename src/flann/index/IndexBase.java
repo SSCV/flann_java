ee0bc425d4b9bb8c96070199796ae9d045590ace
SSCV <visionscience21@gmail.com>
2013-01-15 23:56:37 +0100

    Completed LSHTable. Started IndexLSH.

--- src/flann/index/IndexBase.java
+++ src/flann/index/IndexBase.java
@@ -3,10 +3,12 @@ package flann.index;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import flann.metric.*;
-import flann.result_set.*;
-
-
+import flann.metric.Metric;
+import flann.result_set.CountRadiusResultSet;
+import flann.result_set.KNNRadiusResultSet;
+import flann.result_set.KNNSimpleResultSet;
+import flann.result_set.RadiusResultSet;
+import flann.result_set.ResultSet;
 
 /**
  * Base class for all index structures.
@@ -14,17 +16,23 @@ import flann.result_set.*;
 public abstract class IndexBase {
 	protected Metric metric;
 	protected double[][] data;
+	protected int[][] dataBinary; // binary feature vectors
 	ArrayList<Integer> objectsIndices;
 
 	public int numberOfObjects;
 	public int numberOfDimensions;
 
-	public IndexBase (Metric metric, double[][] data) {
+	public IndexBase(Metric metric, double[][] data) {
 		this.metric = metric;
-		setDataset (data);
+		setDataset(data);
 	}
 
-	protected void setDataset (double[][] data) {
+	public IndexBase(Metric metric, int[][] dataBinary) {
+		this.metric = metric;
+		setDataset(dataBinary);
+	}
+
+	private void setDataset(double[][] data) {
 		if (data.length == 0)
 			return;
 
@@ -40,36 +48,58 @@ public abstract class IndexBase {
 		}
 	}
 
+	private void setDataset(int[][] dataBinary) {
+		if (data.length == 0)
+			return;
+
+		numberOfObjects = data.length;
+		numberOfDimensions = data[0].length;
+
+		// Copy data.
+		this.dataBinary = new int[numberOfObjects][numberOfDimensions];
+		for (int i = 0; i < numberOfObjects; i++) {
+			for (int j = 0; j < numberOfDimensions; j++) {
+				this.dataBinary[i][j] = dataBinary[i][j];
+			}
+		}
+	}
+
 	/**
 	 * Hyperplane partitioning. Subdivide the list of points by a plane
-	 * perpendicular to the axis corresponding to 'cutDimension' at value 'cutValue'.
-	 * On return:
+	 * perpendicular to the axis corresponding to 'cutDimension' at value
+	 * 'cutValue'. On return:
 	 * data[objectsIndices.get(start..start+lim1-1)][cutDimension] < cutValue
-	 * data[objectsIndices.get(start+lim1..start+lim2-1)][cutDimension] == cutValue
-	 * data[objectsIndices.get(start+lim2..start + count - 1)][cutDimension] > cutValue
+	 * data[objectsIndices.get(start+lim1..start+lim2-1)][cutDimension] ==
+	 * cutValue data[objectsIndices.get(start+lim2..start + count -
+	 * 1)][cutDimension] > cutValue
 	 */
-	protected void planeSplit (int start, int count, int cutDimension, double cutValue, int[] lim1Andlim2Wrapper) {
+	protected void planeSplit(int start, int count, int cutDimension,
+			double cutValue, int[] lim1Andlim2Wrapper) {
 		int lim1, lim2;
 		int left = start;
 		int right = start + count - 1;
 		for (;;) {
-			while (left <= right && data[objectsIndices.get(left)][cutDimension] < cutValue)
+			while (left <= right
+					&& data[objectsIndices.get(left)][cutDimension] < cutValue)
 				left++;
-			while (left <= right && data[objectsIndices.get(right)][cutDimension] >= cutValue)
-				right--; 
+			while (left <= right
+					&& data[objectsIndices.get(right)][cutDimension] >= cutValue)
+				right--;
 			if (left > right)
 				break;
 			Collections.swap(objectsIndices, left, right);
 			left++;
 			right--;
 		}
-		
+
 		lim1 = left;
 		right = start + count - 1;
 		for (;;) {
-			while (left <= right && data[objectsIndices.get(left)][cutDimension] <= cutValue)
+			while (left <= right
+					&& data[objectsIndices.get(left)][cutDimension] <= cutValue)
 				left++;
-			while (left <= right && data[objectsIndices.get(right)][cutDimension] > cutValue)
+			while (left <= right
+					&& data[objectsIndices.get(right)][cutDimension] > cutValue)
 				right--;
 			if (left > right)
 				break;
@@ -78,28 +108,31 @@ public abstract class IndexBase {
 			right--;
 		}
 		lim2 = left;
-		
-		// Convert 'lim1' and 'lim2' from absolute index values in 'objectsIndices',
+
+		// Convert 'lim1' and 'lim2' from absolute index values in
+		// 'objectsIndices',
 		// to relative to 'start'.
 		lim1 -= start;
 		lim2 -= start;
-		
+
 		lim1Andlim2Wrapper[0] = lim1;
 		lim1Andlim2Wrapper[1] = lim2;
 	}
-	
-	public void knnSearch (double[][] queries, int[][] indices, double[][] distances, SearchParamsBase searchParams) {
+
+	public void knnSearch(double[][] queries, int[][] indices,
+			double[][] distances, SearchParamsBase searchParams) {
 		int k = searchParams.maxNeighbors;
-		KNNSimpleResultSet resultSet = new KNNSimpleResultSet (k);
+		KNNSimpleResultSet resultSet = new KNNSimpleResultSet(k);
 		for (int i = 0; i < queries.length; i++) {
 			resultSet.clear();
-			findNeighbors (resultSet, queries[i], searchParams);
-			int n = Math.min (resultSet.size(), k);
-			resultSet.copy (distances[i], indices[i], n);
+			findNeighbors(resultSet, queries[i], searchParams);
+			int n = Math.min(resultSet.size(), k);
+			resultSet.copy(distances[i], indices[i], n);
 		}
-    }
+	}
 
-	public int radiusSearch (double[][] queries, int[][] indices, double[][] distances, SearchParamsBase searchParams) {
+	public int radiusSearch(double[][] queries, int[][] indices,
+			double[][] distances, SearchParamsBase searchParams) {
 		double radius = searchParams.radius;
 		int maxNeighbors = searchParams.maxNeighbors;
 		int outputColumns = indices[0].length;
@@ -113,39 +146,41 @@ public abstract class IndexBase {
 		if (maxNeighbors == 0) {
 			CountRadiusResultSet resultSet = new CountRadiusResultSet(radius);
 			for (int i = 0; i < queries.length; i++) {
-				resultSet.clear ();
-				findNeighbors (resultSet, queries[i], searchParams);
+				resultSet.clear();
+				findNeighbors(resultSet, queries[i], searchParams);
 				count += resultSet.size();
 			}
 		} else {
 			// Unlimited result-set.
-			if (searchParams.maxNeighbors < 0 && numberOfObjects <= outputColumns) {
-				RadiusResultSet resultSet = new RadiusResultSet (radius);
+			if (searchParams.maxNeighbors < 0
+					&& numberOfObjects <= outputColumns) {
+				RadiusResultSet resultSet = new RadiusResultSet(radius);
 				for (int i = 0; i < queries.length; i++) {
-					resultSet.clear ();
-					findNeighbors (resultSet, queries[i], searchParams);
+					resultSet.clear();
+					findNeighbors(resultSet, queries[i], searchParams);
 					int n = resultSet.size();
 					count += n;
-					n = Math.min (n, outputColumns);
-					resultSet.copy (distances[i], indices[i], n);
-					
+					n = Math.min(n, outputColumns);
+					resultSet.copy(distances[i], indices[i], n);
+
 					// Mark the position after the last element as unused.
 					if (n < outputColumns) {
 						distances[i][n] = Double.MAX_VALUE;
 						indices[i][n] = -1;
 					}
 				}
-			// Limited result-set.
+				// Limited result-set.
 			} else {
-				KNNRadiusResultSet resultSet = new KNNRadiusResultSet (radius, maxNeighbors);
+				KNNRadiusResultSet resultSet = new KNNRadiusResultSet(radius,
+						maxNeighbors);
 				for (int i = 0; i < queries.length; i++) {
-					resultSet.clear ();
-					findNeighbors (resultSet, queries[i], searchParams);
+					resultSet.clear();
+					findNeighbors(resultSet, queries[i], searchParams);
 					int n = resultSet.size();
 					count += n;
 					n = Math.min(n, maxNeighbors);
-					resultSet.copy (distances[i], indices[i], n);
-					
+					resultSet.copy(distances[i], indices[i], n);
+
 					// Mark the position after the last element as unused.
 					if (n < outputColumns) {
 						distances[i][n] = Double.MAX_VALUE;
@@ -154,15 +189,16 @@ public abstract class IndexBase {
 				}
 			}
 		}
-		
+
 		return count;
 	}
-	
-	public void buildIndex () {
-		buildIndexImpl ();
-	}
-	
-	protected abstract void buildIndexImpl ();
 
-	protected abstract void findNeighbors (ResultSet resultSet, double[] query, SearchParamsBase searchParams);
+	public void buildIndex() {
+		buildIndexImpl();
+	}
+
+	protected abstract void buildIndexImpl();
+
+	protected abstract void findNeighbors(ResultSet resultSet, double[] query,
+			SearchParamsBase searchParams);
 }
--- src/flann/index/IndexLSH.java
+++ src/flann/index/IndexLSH.java
@@ -1,190 +1,53 @@
 package flann.index;
 
 import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import flann.metric.Metric;
 import flann.result_set.ResultSet;
 
 public class IndexLSH extends IndexBase {
-	// Hash tables, one optimized for speed, the other for space.
-	BucketsSpeed bucketsSpeed;
-	BucketsSpace bucketsSpace;
-
-	// Decides what will be used to store/manage the data.
-	SpeedLevel speedLevel;
-
-	// A speedup so that we don't look in the hash table,
-	// which can be much slower than checking a bitset.
-	BitSet keyBitSet;
-
-	// Size of the key in bits, used in the hash tables.
+	ArrayList<LSHTable> tables;
+	int tablesNumber;
 	int keySize;
 
-	// The mask to apply to a point to get the hask key.
-	ArrayList<Integer> mask;
+	// How far should we look for neighbors in multi-probe LSH.
+	int multiProbeLevel;
 
-	/**
-	 * Objects of this type are stored in an LSH bucket.
-	 */
-	private class PointIndex {
-		public int val;
+	// The XOR masks to apply to a key to get the neighboring buckets.
+	ArrayList<Integer> xorMasks;
 
-		public PointIndex(int val) {
-			this.val = val;
+	public static class BuildParams {
+		public int tablesNumber;
+		public int keySize;
+		public int multiProbeLevel;
+
+		public BuildParams() {
+			this.tablesNumber = 12;
+			this.keySize = 20;
+			this.multiProbeLevel = 2;
+		}
+
+		public BuildParams(int tablesNumber, int keySize, int multiProbeLevel) {
+			this.tablesNumber = tablesNumber;
+			this.keySize = keySize;
+			this.multiProbeLevel = multiProbeLevel;
 		}
 	}
 
-	/**
-	 * The id from which we can get a bucket in an LSH table.
-	 */
-	private class BucketKey {
-		public int val;
-
-		public BucketKey(int val) {
-			this.val = val;
-		}
-	}
-
-	/**
-	 * A bucket in an LSH table.
-	 */
-	private class Bucket {
-		public ArrayList<PointIndex> points = new ArrayList<PointIndex>();
-	}
-
-	/**
-	 * A hash table which contains buckets, and each bucket is indexed by a
-	 * bucket key. Optimized for space.
-	 */
-	private class BucketsSpace {
-		HashMap<BucketKey, Bucket> buckets = new HashMap<BucketKey, Bucket>();
-	}
-
-	/**
-	 * A hash table which contains buckets, and each bucket is indexed by a
-	 * bucket key. Optimized for speed.
-	 */
-	private class BucketsSpeed {
-		ArrayList<Bucket> buckets = new ArrayList<Bucket>();
-	}
-
-	/**
-	 * Defines the speed for the implementation. kArray uses an ArrayList for
-	 * storing data. kBitsetHash uses a hash map but checks for the validity of
-	 * a key with a bitset. kHash uses a hash map only.
-	 */
-	enum SpeedLevel {
-		kArray, kBitsetHash, kHash
-	};
-
-	public void add(int pointIndex, int[] point) {
-		BucketKey key = new BucketKey(1);
-		int k = key.val;
-		PointIndex p = new PointIndex(k);
-
-		switch (speedLevel) {
-		case kArray:
-			bucketsSpeed.buckets.get(k).points.add(p);
-			break;
-		case kBitsetHash:
-			keyBitSet.set(k);
-			bucketsSpace.buckets.get(k).points.add(p);
-			break;
-		case kHash:
-			bucketsSpace.buckets.get(k).points.add(p);
-			break;
-		}
-	}
-
-	private class PointIndexPointPair {
-		int pointIndex;
-		int[] point;
-	}
-
-	public void add(ArrayList<PointIndexPointPair> points) {
-		for (int i = 0; i < points.size(); i++) {
-			add(points.get(i).pointIndex, points.get(i).point);
-		}
-		// optimize();
-	}
-
-	private Bucket getBucketFromKey(BucketKey key) {
-		int k = key.val;
-
-		switch (speedLevel) {
-		case kArray:
-			return bucketsSpeed.buckets.get(k);
-		case kBitsetHash:
-			if (keyBitSet.get(k))
-				return bucketsSpace.buckets.get(k);
-			else
-				return null;
-		case kHash:
-			return bucketsSpace.buckets.get(k);
-		}
-
-		return null;
-	}
-
-	public int getKey(int[] point) {
-		return 1;
+	public static class SearchParams extends SearchParamsBase {
 	}
 
 	public IndexLSH(Metric metric, double[][] data) {
 		super(metric, data);
-
-		// Initialize bucketsSpeed.
-		int bucketsSpeedSize = (1 << keySize);
-		for (int i = 0; i < bucketsSpeedSize; i++) {
-			bucketsSpeed.buckets.add(new Bucket());
-		}
-	}
-
-	private void optimize() {
-		// If we are already using fast storage, don't do anything.
-		if (speedLevel == SpeedLevel.kArray)
-			return;
-
-		if (bucketsSpace.buckets.size() > (1 << keySize) / 2) {
-			speedLevel = SpeedLevel.kArray;
-			int size = 1 << keySize;
-			bucketsSpeed.buckets.clear();
-			for (int i = 0; i < size; i++) {
-				bucketsSpeed.buckets.add(null);
-			}
-			Set<Entry<BucketKey, Bucket>> s = bucketsSpace.buckets.entrySet();
-			for (Entry<BucketKey, Bucket> e : s) {
-				int key = e.getKey().val;
-				Bucket b = e.getValue();
-				bucketsSpeed.buckets.set(key, b);
-			}
-			bucketsSpace.buckets.clear();
-			return;
-		}
-
-		if (false) {
-			// TODO: Not sure about this...
-		} else {
-			speedLevel = SpeedLevel.kHash;
-			keyBitSet.clear();
-		}
 	}
 
 	@Override
 	protected void buildIndexImpl() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	protected void findNeighbors(ResultSet resultSet, double[] query,
 			SearchParamsBase searchParams) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
